52a0d2738cac2494af974b130b481ad7d78c0891
Samyukth <vinodsamyukth@gmail.com>
2023-10-10 11:21:46 +0530

    astronomy classes defined

---
+++ imports/classes/slotCategory.js
@@ -0,0 +1,45 @@
+import { Class } from 'meteor/jagi:astronomy';
+import Slots from "../collection/slotsCollection"
+
+
+
+const singleSlot = Class.create({
+    name: singleSlot,
+    fields: {
+        seatNumber: {
+            type: Number
+        },
+        bookingStatus: {
+            type: Boolean
+        },
+        bookedUser: {
+            type: String
+        }
+
+
+    }
+})
+
+
+const Slot = Class.create({
+    name: "Slot",
+    collection: Slots,
+    fields: {
+        category: String,
+        price: Number,
+        seatCount: Number,
+        singleSeat: {
+            type: [singleSlot],
+            default: function () {
+                return [];
+            }
+
+        }
+
+
+    }
+
+});
+
+
+export default Slot
